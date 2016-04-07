3d69f7ddccbb110db7e029d76482493ff1ba1df0
serknight <chrisknight.mail@gmail.com>
2016-04-07 10:50:24 -0400

    nodef

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ActiveRecord::Base
 	has_and_belongs_to_many :artists
 	has_many :gallery_images
 
-	has_attached_file :hero, styles: { large: "1280x1024>", medium: "650x650" }, default_url: "/images/codez.jpg"
+	has_attached_file :hero, styles: { large: "1280x1024>", medium: "650x650" }
   validates_attachment_content_type :hero, content_type: /\Aimage\/.*\Z/
 
 
