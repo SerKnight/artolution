8ce638abb8d37b1fbee88f856dd6e74dcc837a2c
serknight <chrisknight.mail@gmail.com>
2016-04-12 19:49:04 -0400

    has attached cred

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,9 +2,7 @@ class Project < ActiveRecord::Base
 	has_and_belongs_to_many :artists
 	has_many :gallery_images
 
-	has_attached_file :hero, styles: { large: "1280x1024>", medium: "650x650" },
-	:url =>':s3_domain_url',
-  :path => '/:class/:attachment/:id_partition/:style/:filename',
+	has_attached_file :hero, styles: { large: "1280x1024>", medium: "650x650" }, :url =>':s3_domain_url', :path => '/:class/:attachment/:id_partition/:style/:filename'
   validates_attachment_content_type :hero, content_type: /\Aimage\/.*\Z/
 
 
