ee58052e8be490fd9a3867c2189b76a300292786
serknight <chrisknight.mail@gmail.com>
2016-04-12 22:42:45 -0400

    s3_region

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,6 @@ Rails.application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :region => 'us-east-1',
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
@@ -43,7 +42,7 @@ Rails.application.configure do
     :s3_host_alias => 'static.artolution.com',
     :path => ":class/:attachment/:id_partition/:style/:filename",
     :bucket => ENV['S3_BUCKET_NAME'],
-    :region => 'us-east-1',
+    :s3_region => 'us-east-1',
     :s3_host_name => 's3.amazonaws.com'
   }
 
