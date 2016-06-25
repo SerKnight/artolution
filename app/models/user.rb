9de5b3652d4172a74272fcda6e89638debc27012
serknight <chrisknight.mail@gmail.com>
2016-06-25 15:42:43 -0400

    ugh hate heroku assets

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,4 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-
-   include MailForm::Delivery
-
-   append :remote_ip, :user_agent, :session
-   attributes :name, :email, :created_at
-
-   def headers
-     {
-       :to => "your.email@your.domain.com",
-       :subject => "User created an account"
-     }
-   end
 end
