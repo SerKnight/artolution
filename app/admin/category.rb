0f9d59fc422ddb6faf95d9c5d170808decb3d0be
serknight <chrisknight.mail@gmail.com>
2016-10-26 00:25:17 -0400

    category

--- app/admin/category.rb
+++ app/admin/category.rb
@@ -1,17 +1,14 @@
-ActiveAdmin.register Category do
+ActiveAdmin.register Category, :as => "project_category" do
 
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+	controller do
+	  def find_resource
+	    scoped_collection.friendly.find(params[:id])
+	  end
+	  def permitted_params
+	  	params.permit!
+	  end
+	end
+
 
 
 end
