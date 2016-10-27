b2a2a7dcd9975aa5c365b5a3a2e07b0b87d8c8ed
serknight <chrisknight.mail@gmail.com>
2016-10-26 21:31:59 -0400

    assets

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module Artolution
     # config.time_zone = 'Central Time (US & Canada)'
     config.active_record.raise_in_transactional_callbacks = true
     
-    config.assets.initialize_on_precompile = false
+    # config.assets.initialize_on_precompile = false
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
